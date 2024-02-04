06cd224db82b35d027ff7f7ee346b0c71a14e0da
boryanayordanova <boryana.yourdanova@gmail.com>
2024-02-04 02:43:42 +0200

    uploading to db

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,5 +1,6 @@
 'use client';
 import { useRef, useState } from 'react';
+import Image from 'next/image';
 
 import classes from './image-picker.module.css';
 
@@ -10,18 +11,20 @@ export default function ImagePicker({label, name}){
 
     function handlePickClick(){
         imageInput.current.click();
+        console.log('aaa');
     }
 
     function handleImageChange(event){
         const file = event.target.files[0];
 
         if(!file){
+            setPickedImage(null);
             return;
         }
 
         const fileReader = new FileReader();
         fileReader.onload = () => {
-            
+            setPickedImage(fileReader.result);
         };
         fileReader.readAsDataURL(file);
     }
@@ -32,6 +35,15 @@ export default function ImagePicker({label, name}){
                 {label}
             </label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && 
+                        <Image 
+                            src={pickedImage} 
+                            alt="The image selected by the user." 
+                            fill 
+                        />}
+                </div>
                 <input 
                     className={classes.input}
                     type="file" 
@@ -40,8 +52,13 @@ export default function ImagePicker({label, name}){
                     name={name}
                     ref={imageInput}
                     onChange={handleImageChange}
+                    required
                 />
-                <button className={classes.button} type="button" onCLilck={handlePickClick}>
+                <button 
+                    className={classes.button} 
+                    type="button" 
+                    onClick={handlePickClick}
+                >
                     Pick an Image
                 </button>
             </div>
